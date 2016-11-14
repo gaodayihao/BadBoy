1236862bbbf495738dcaeff6b5a31df23bc5594f
gaodayihao <ellis_loveless@qq.com>
2016-11-15 02:39:31 +0800

    漏掉的简体中文文件

--- Localization/localization_cn.lua
+++ Localization/localization_cn.lua
@@ -6,17 +6,28 @@ LC_UNITS_TPS = "TPS：单位扫描";
 LC_UNITS_TPS_DESCRIPTION = "设置每秒对附近单位的分析次数";
 
 --RotationOptions Common
-LC_GENERAL = "一般";
+LC_GENERAL = "常规";
 LC_DEFENSIVE = "自保";
 LC_PRE_PULL_BOSSMOD = "BOSS开怪前准备";
 LC_POTION = "爆发药水";
-LC_OLD_WAR = "|cffFFFF00上古战神药水";
-LC_PROLONGED_POWER = "|cffFF0000延时之力药水";
+LC_OLD_WAR = "|cff00BB00上古战神";
+LC_DEADLY_GRACE = "|cffFFFF00致命优雅";
+LC_PROLONGED_POWER = "|cff00BB00延时之力";
+LC_FLASK = "合剂";
+LC_DOT_MINIMUM_HEALTH = "Dot最小血量";
+LC_DOT_MINIMUM_HEALTH_DESCRIPTION = "|cffFFBB00多线输出下Dot施放最小血量（百万）";
+LC_PRE_PULL_TIMER = "开怪倒计时";
+LC_PRE_PULL_TIMER_DESCRIPTION =  "|cffFFFFFF在开怪倒计时到达多少秒时进行准备（需要DBM）";
 
+--Priest
+LC_BODY_AND_SOUL = "身心合一";
+LC_BODY_AND_SOUL_DESCRIPTION = "|cffFFBB00在玩家移动时，延时n秒使用|cffFF0000圣言术：盾|cffFFBB00来触发身心合一。";
 --Shawdow Priest
 LC_SWP_MAX_TARGETS = "暗言术：痛 目标上限";
 LC_SWP_MAX_TARGETS_DESCRIPTION = "|cffFFBB00多线输出时，痛的目标上限";
 LC_VT_MAX_TARGETS = "吸血之触 目标上限";
 LC_VT_MAX_TARGETS_DESCRIPTION = "|cffFFBB00多线输出时，触的目标上限";
+LC_VAMPIRIC_EMBRACE = "吸血鬼的拥抱";
+LC_VAMPIRIC_EMBRACE_DESCRIPTION = "|cffFFBB00当血量低于设定百分比时使用|cffFF0000吸血鬼的拥抱";
 
 end
